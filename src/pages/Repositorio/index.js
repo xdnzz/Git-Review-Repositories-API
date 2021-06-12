db9cda591feb41be0a70e00f14c1b0c00dce21dd
Danilo Diniz <danilosdinizz@gmail.com>
2021-06-12 10:35:54 -0300

    aplicação para consumir API do GITHUB

---
+++ src/pages/Repositorio/index.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import {Container} from './styles';
+import api from '../../services/api';
+
+export default function Repositorio({match}){
+
+  const [repositorio, setRepositorio] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    
+    async function load(){
+      const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params:{
+            state: 'open',
+            per_page: 5
+          }
+         })
+      ]);
+
+      console.log(repositorioData.data);
+      console.log(issuesData.data);
+
+      setRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+
+    }
+
+    load();
+
+  }, [match.params.repositorio]);
+
+  return(
+    <Container>
+
+    </Container>
+  )
+}
