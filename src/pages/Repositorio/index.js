dd1940f7dc06c1cc93df74f0d89add3b87d8ac6a
Danilo Diniz <danilosdinizz@gmail.com>
2021-06-14 22:49:58 -0300

    atualizacao

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import {Container} from './styles';
+import {Container, Owner, Loading, BackButton, IssuesList, PageActions} from './styles';
+import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
 export default function Repositorio({match}){
@@ -7,6 +8,7 @@ export default function Repositorio({match}){
   const [repositorio, setRepositorio] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(()=> {
     
@@ -20,14 +22,13 @@ export default function Repositorio({match}){
             state: 'open',
             per_page: 5
           }
-         })
+        })
       ]);
-
-      console.log(repositorioData.data);
-      console.log(issuesData.data);
 
       setRepositorio(repositorioData.data);
       setIssues(issuesData.data);
+      console.log(issuesData.data);
+
       setLoading(false);
 
     }
@@ -36,8 +37,92 @@ export default function Repositorio({match}){
 
   }, [match.params.repositorio]);
 
+
+  useEffect(()=> {
+
+    async function loadIssue(){
+      const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params:{
+          state: 'open',
+          page,
+          per_page: 5,
+        },
+      });
+
+      setIssues(response.data);
+
+    }
+
+    loadIssue();
+
+  }, [match.params.repositorio, page]);
+  
+  function handlePage(action){
+    setPage(action === 'back' ? page - 1 : page + 1 )
+  }
+
+
+  if(loading){
+    return(
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    )
+  }
+  
   return(
     <Container>
+        <BackButton to="/">
+          <FaArrowLeft color="#000" size={30} />
+        </BackButton>
+
+        <Owner>
+          <img 
+          src={repositorio.owner.avatar_url} 
+          alt={repositorio.owner.login} 
+          />
+          <h1>{repositorio.name}</h1>
+          <p>{repositorio.description}</p>
+        </Owner>
+
+        <IssuesList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+
+                </strong>
+
+                <p>{issue.user.login}</p>
+
+              </div>
+
+            </li>
+          ))}
+        </IssuesList>
+
+        <PageActions>
+          <button 
+          type="button" 
+          onClick={()=> handlePage('back') }
+          disabled={page < 2}
+          >
+            Voltar
+          </button>
+
+          <button type="button" onClick={()=> handlePage('next') }>
+            Proxima
+          </button>
+        </PageActions>
 
     </Container>
   )
